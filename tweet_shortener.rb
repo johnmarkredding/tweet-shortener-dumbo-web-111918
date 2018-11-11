e5ef416c4ed009dd7f7b4d0e432a5f6f3beabd80
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 04:26:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 
-short_hash = {"hello" => 'hi'
-"to, two, too" => '2' 
-"for, four" => '4'
-'be' => 'b'
-'you' => 'u'
-"at" => "@" 
+short_hash = {"hello" => 'hi',
+"to, two, too" => '2',
+"for, four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@" ,
 "and" => "&" }
