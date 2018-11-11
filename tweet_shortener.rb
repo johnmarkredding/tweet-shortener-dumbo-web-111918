a16a1c000599afb5d035b2129609a43abc96f2cb
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 05:13:52 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,46 @@
 # Write your code here.
+
+def word_substituter(str)
+  short_hash = {
+    " hello " => ' hi ',
+    " to " => ' 2 ',
+    " two " => ' 2 ',
+    " too " => ' 2 ',
+    " for " => ' 4 ',
+    " For " => ' 4 ',
+    " four " => ' 4 ',
+    " be " => ' b ',
+    " you " => ' u ',
+    " at " => " @ " ,
+    " and " => " & "
+  }
+  short_hash.each {|k,v|
+    str.gsub!(k, v)
+  }
+  str
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each {|tweet|
+    puts word_substituter(tweet)
+  }
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.size > 140
+    word_substituter(tweet)
+  end
+  tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.size >= 140
+    short_tweet = word_substituter(tweet)
+    if short_tweet.size > 140
+      short_tweet = short_tweet[0, 140]
+    end
+    short_tweet
+  else
+    tweet
+  end
+end
