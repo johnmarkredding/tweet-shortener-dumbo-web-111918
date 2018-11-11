8f6517715853fcd61399735361aab09107625259
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 05:13:00 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,7 +38,8 @@ def shortened_tweet_truncator(tweet)
     short_tweet = word_substituter(tweet)
     if short_tweet.size > 140
       short_tweet = short_tweet[0, 140]
-    end 
+    end
+    short_tweet
   else
     tweet
   end
