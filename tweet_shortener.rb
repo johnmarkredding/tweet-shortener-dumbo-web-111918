36ee1ca48427ca1fb8dbfef61e193b2026379575
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 05:03:15 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -7,6 +7,7 @@ def word_substituter(str)
     " two " => ' 2 ',
     " too " => ' 2 ',
     " for " => ' 4 ',
+    " For " => ' 4 ',
     " four " => ' 4 ',
     " be " => ' b ',
     " you " => ' u ',
